a0796d54644d2936146a6cc4a4db7cac48b5347e
Juan Arellano <jarellano0438@vbtc.net>
2017-03-21 13:26:19 -0400

    I'm not like Adam

--- application/application/application.cpp
+++ application/application/application.cpp
@@ -29,3 +29,19 @@ int main()
 	return 0;
 }
 
+//return a jumbled version of aWord
+string mixUp(const string& aWord)
+{
+	string aJumble = aWord;
+	int length = static_cast<int>(aJumble.size());
+
+	for (int i = 0; i < length; ++i)
+	{
+		int randomIndex = (rand() % length);
+		char temp = aJumble[i];
+		aJumble[i] = aJumble[randomIndex];
+		aJumble[randomIndex] = temp;
+	}
+	return aJumble;
+}
+
