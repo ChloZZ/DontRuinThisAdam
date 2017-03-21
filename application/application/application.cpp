86997fefa529cec869d0dabe0927ed50a97210e7
Marcus Secondi <marcussecondi@yahoo.com>
2017-03-21 13:28:59 -0400

    fts!

--- application/application/application.cpp
+++ application/application/application.cpp
@@ -29,3 +29,19 @@ int main()
 	return 0;
 }
 
+void play(const string& aWord)
+{
+	string guess;
+	do
+	{
+		cout << endl << endl << "Your guess: ";
+		cin >> guess;
+		if ((guess != aWord) && (guess != "quit"))
+		{
+			cout << "Sorry, that's not it.";
+		}
+	} while ((guess != aWord) && (guess != "quit"));
+	if (guess == aWord)
+		cout << endl << "That's it! You guessed it!" << endl;
+	cout << endl << "Thanks for playing.\n" << endl;
+}
