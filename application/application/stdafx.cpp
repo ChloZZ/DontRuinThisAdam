f948de28ac1589a5c0c7055e952e808da42656db
Potatofamine <cmccarthy0674@vbtc.net>
2017-03-22 13:36:58 -0400

    NA

--- application/application/stdafx.cpp
+++ application/application/stdafx.cpp
@@ -1,9 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-<<<<<<< HEAD:application/application/stdafx.cpp
 // application.pch will be the pre-compiled header
-=======
-// application - real.pch will be the pre-compiled header
->>>>>>> 587543ccee40994f408b9a678fb54d1fe75b771e:application/application - real/stdafx.cpp
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
