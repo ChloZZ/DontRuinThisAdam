2b4959ba0fe7d8211de052bfccc01d5e734f5b90
Potatofamine <cmccarthy0674@vbtc.net>
2017-03-22 13:35:36 -0400

    Rename fix.

--- application/application/application.cpp
+++ application/application/application.cpp
@@ -27,22 +27,3 @@ int main()
 
 	return 0;
 }
-
-//return a random word
-string randomWord()
-{
-	const int NUM_WORDS = 5;
-	const string WORDS[NUM_WORDS] =
-	{
-		"wall",
-		"glasses",
-		"labored",
-		"persistent",
-		"jumble"
-	};
-
-	int choice = (rand()) % NUM_WORDS);
-	string word = WORDS[choice];
-
-	return word;
-}
