fba0b17abb9c7b614196b6b0f235a6424183576f
Marcus Secondi <marcussecondi@yahoo.com>
2017-03-24 12:50:37 -0400

    Fixed

--- application/application/application.cpp
+++ application/application/application.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Chapter 4, Project 4
 // Word Jumble - with functions
 
@@ -54,52 +53,15 @@ string mixUp(const string& aWord)
 	return string();
 }
 
-void welcomePlayer(const string& aJumble)
-{
-
-}
-
 
 void play(const string& aWord)
 {
 
 }
-=======
-// Chapter 4, Project 4
-// Word Jumble - with functions
 
-
-#include <iostream>
-#include <string>
-//#include <cstdlib>
-#include <time.h>
-
-using namespace std;
-
-string randomWord();
-string mixUp(const string& aWord);
-void welcomePlayer(const string& aJumble);
-void play(const string& aWord);
-
-int main()
-{
-	srand(static_cast<unsigned int>(time(0)));
-
-	string word = randomWord();
-	string jumble = mixUp(word);
-	welcomePlayer(jumble);
-	play(word);
-
-	system("pause");
-
-
-	return 0;
-}
 
 void welcomePlayer(const string& aJumble)
 {
 	cout << "Welcome to a console application where a word is scrambled and the player who is reading this must unscramble said word..." << endl << endl;
 	cout << "You are to unscramble: " << aJumble << endl;
 }
-
->>>>>>> 8a421e6ee0797cdd7374c6693a559bc096811c0b
