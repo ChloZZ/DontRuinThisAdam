d01383f1f753eb52a303247bd4022e51a7cb4649
Juan Arellano <jarellano0438@vbtc.net>
2017-03-24 13:40:01 -0400

    Final revision

--- application/application/application.cpp
+++ application/application/application.cpp
@@ -48,6 +48,22 @@ string randomWord()
 	return word;
 }
 
+//return a jumbled version of aWord
+string mixUp(const string& aWord)
+{
+	string aJumble = aWord;
+	int length = static_cast<int>(aJumble.size());
+
+	for (int i = 0; i < length; ++i)
+	{
+		int randomIndex = (rand() % length);
+		char temp = aJumble[i];
+		aJumble[i] = aJumble[randomIndex];
+		aJumble[randomIndex] = temp;
+	}
+	return aJumble;
+}
+
 
 void welcomePlayer(const string& aJumble)
 {
